12fc0ef7bbbd889e2f3343579c76c41febb6f6eb
Thawinwats Uggaravisee <thawinwats@sirimedia.com>
2019-03-26 17:42:35 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Mitr');
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Mitr', sans-serif;
+  background: black;
+}
+`
+
+const theme = {
+  primary: '#a04ed9'
+}
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <React.Fragment>
+      <GlobalStyle />
+      <App />
+    </React.Fragment>
+  </ThemeProvider>,
+  document.getElementById('root')
+)
