954abf838a926b60338bd5d633bb3c45491dffbd
Thawinwats Uggaravisee <thawinwats@sirimedia.com>
2019-03-27 15:26:35 +0700

    Adding CarInfo

--- src/App.js
+++ src/App.js
@@ -64,6 +64,39 @@ const getCarImage = (color, name) => {
   return images
 }
 
+const carColorHandler = (color) => {
+  switch (color) {
+    case 'red':
+      return { name: 'Tango Red metallic', price: 575 }
+    case 'gray':
+      return { name: 'Daytona Gray pearl', price: 1075 }
+
+    default:
+      break
+  }
+}
+
+const CarInfoStyles = styled.div`
+  p {
+    margin: 0;
+    font-size: 0.95rem;
+  }
+  span {
+    font-size: 1.2rem;
+  }
+`
+
+const CarInfo = (props) => {
+  const { name, price } = carColorHandler(props.color)
+
+  return (
+    <CarInfoStyles>
+      <p>{name}</p>
+      <span>${price}</span>
+    </CarInfoStyles>
+  )
+}
+
 const App = () => {
   const [ color, setColor ] = useState('red')
 
@@ -83,6 +116,7 @@ const App = () => {
           {color === 'gray' && <ExteriorColorChecked />}
         </li>
       </ExteriorColor>
+      <CarInfo color={color} />
     </div>
   )
 }
