fb11c2bea169e2f5e15e65734f3f6118f59382f6
Thawinwats Uggaravisee <thawinwats@sirimedia.com>
2019-03-29 11:05:42 +0700

    Edit db.js

--- src/db/db.js
+++ src/db/db.js
@@ -1,43 +1,46 @@
 import { baseUrl } from '../config'
 
-const carColorSetUrl = `${baseUrl}/images/car/colorSet`
+const carColorSetPath = `${baseUrl}/images/car/colorSet`
+const wheelsTypePath = `${baseUrl}/images/wheels`
 
 export const cars = [
   {
     id: 0,
     name: 'Tango Red metallic',
     price: 575,
-    tilesImage: `${carColorSetUrl}/tango-red-metallic/tiles.jpg`,
+    tilesImage: `${carColorSetPath}/tango-red-metallic/tiles.jpg`,
     wheels: [
       {
         id: 0,
         name: '19in',
         price: 0,
         description: '19" 5-arm-V-spoke design High-gloss Black cast wheels',
+        typeImage: `${wheelsTypePath}/19in.png`,
         images: [
-          `${carColorSetUrl}/tango-red-metallic/wheels/19in/1.jpeg`,
-          `${carColorSetUrl}/tango-red-metallic/wheels/19in/2.jpeg`,
-          `${carColorSetUrl}/tango-red-metallic/wheels/19in/3.jpeg`,
-          `${carColorSetUrl}/tango-red-metallic/wheels/19in/4.jpeg`,
-          `${carColorSetUrl}/tango-red-metallic/wheels/19in/5.jpeg`,
-          `${carColorSetUrl}/tango-red-metallic/wheels/19in/6.jpeg`,
-          `${carColorSetUrl}/tango-red-metallic/wheels/19in/7.jpeg`
+          `${carColorSetPath}/tango-red-metallic/wheels/19in/1.jpeg`,
+          `${carColorSetPath}/tango-red-metallic/wheels/19in/2.jpeg`,
+          `${carColorSetPath}/tango-red-metallic/wheels/19in/3.jpeg`,
+          `${carColorSetPath}/tango-red-metallic/wheels/19in/4.jpeg`,
+          `${carColorSetPath}/tango-red-metallic/wheels/19in/5.jpeg`,
+          `${carColorSetPath}/tango-red-metallic/wheels/19in/6.jpeg`,
+          `${carColorSetPath}/tango-red-metallic/wheels/19in/7.jpeg`
         ]
       },
       {
         id: 1,
         name: '20in',
         price: 1500,
+        typeImage: `${wheelsTypePath}/20in.jpg`,
         description: '20" 10-Y-spoke design Anthracite finish wheels',
         subDescription: 'with 245/30 front and 305/30 rear tires',
         images: [
-          `${carColorSetUrl}/tango-red-metallic/wheels/20in/1.jpeg`,
-          `${carColorSetUrl}/tango-red-metallic/wheels/20in/2.jpeg`,
-          `${carColorSetUrl}/tango-red-metallic/wheels/20in/3.jpeg`,
-          `${carColorSetUrl}/tango-red-metallic/wheels/20in/4.jpeg`,
-          `${carColorSetUrl}/tango-red-metallic/wheels/20in/5.jpeg`,
-          `${carColorSetUrl}/tango-red-metallic/wheels/20in/6.jpeg`,
-          `${carColorSetUrl}/tango-red-metallic/wheels/20in/7.jpeg`
+          `${carColorSetPath}/tango-red-metallic/wheels/20in/1.jpeg`,
+          `${carColorSetPath}/tango-red-metallic/wheels/20in/2.jpeg`,
+          `${carColorSetPath}/tango-red-metallic/wheels/20in/3.jpeg`,
+          `${carColorSetPath}/tango-red-metallic/wheels/20in/4.jpeg`,
+          `${carColorSetPath}/tango-red-metallic/wheels/20in/5.jpeg`,
+          `${carColorSetPath}/tango-red-metallic/wheels/20in/6.jpeg`,
+          `${carColorSetPath}/tango-red-metallic/wheels/20in/7.jpeg`
         ]
       }
     ]
@@ -46,37 +49,37 @@ export const cars = [
     id: 1,
     name: 'Daytona Gray pearl',
     price: 575,
-    tilesImage: `${carColorSetUrl}/daytona-gray-pearl/tiles.jpg`,
+    tilesImage: `${carColorSetPath}/daytona-gray-pearl/tiles.jpg`,
     wheels: [
       {
         id: 0,
-        name: '19in',
         price: 0,
+        typeImage: `${wheelsTypePath}/19in.png`,
         description: '19" 5-arm-V-spoke design High-gloss Black cast wheels',
         images: [
-          `${carColorSetUrl}/daytona-gray-pearl/wheels/19in/1.jpeg`,
-          `${carColorSetUrl}/daytona-gray-pearl/wheels/19in/2.jpeg`,
-          `${carColorSetUrl}/daytona-gray-pearl/wheels/19in/3.jpeg`,
-          `${carColorSetUrl}/daytona-gray-pearl/wheels/19in/4.jpeg`,
-          `${carColorSetUrl}/daytona-gray-pearl/wheels/19in/5.jpeg`,
-          `${carColorSetUrl}/daytona-gray-pearl/wheels/19in/6.jpeg`,
-          `${carColorSetUrl}/daytona-gray-pearl/wheels/19in/7.jpeg`
+          `${carColorSetPath}/daytona-gray-pearl/wheels/19in/1.jpeg`,
+          `${carColorSetPath}/daytona-gray-pearl/wheels/19in/2.jpeg`,
+          `${carColorSetPath}/daytona-gray-pearl/wheels/19in/3.jpeg`,
+          `${carColorSetPath}/daytona-gray-pearl/wheels/19in/4.jpeg`,
+          `${carColorSetPath}/daytona-gray-pearl/wheels/19in/5.jpeg`,
+          `${carColorSetPath}/daytona-gray-pearl/wheels/19in/6.jpeg`,
+          `${carColorSetPath}/daytona-gray-pearl/wheels/19in/7.jpeg`
         ]
       },
       {
         id: 1,
-        name: '20in',
         price: 1500,
+        typeImage: `${wheelsTypePath}/20in.jpg`,
         description: '20" 10-Y-spoke design Anthracite finish wheels',
         subDescription: 'with 245/30 front and 305/30 rear tires',
         images: [
-          `${carColorSetUrl}/daytona-gray-pearl/wheels/20in/1.jpeg`,
-          `${carColorSetUrl}/daytona-gray-pearl/wheels/20in/2.jpeg`,
-          `${carColorSetUrl}/daytona-gray-pearl/wheels/20in/3.jpeg`,
-          `${carColorSetUrl}/daytona-gray-pearl/wheels/20in/4.jpeg`,
-          `${carColorSetUrl}/daytona-gray-pearl/wheels/20in/5.jpeg`,
-          `${carColorSetUrl}/daytona-gray-pearl/wheels/20in/6.jpeg`,
-          `${carColorSetUrl}/daytona-gray-pearl/wheels/20in/7.jpeg`
+          `${carColorSetPath}/daytona-gray-pearl/wheels/20in/1.jpeg`,
+          `${carColorSetPath}/daytona-gray-pearl/wheels/20in/2.jpeg`,
+          `${carColorSetPath}/daytona-gray-pearl/wheels/20in/3.jpeg`,
+          `${carColorSetPath}/daytona-gray-pearl/wheels/20in/4.jpeg`,
+          `${carColorSetPath}/daytona-gray-pearl/wheels/20in/5.jpeg`,
+          `${carColorSetPath}/daytona-gray-pearl/wheels/20in/6.jpeg`,
+          `${carColorSetPath}/daytona-gray-pearl/wheels/20in/7.jpeg`
         ]
       }
     ]
